b08fe6ce6d3c0878ab7d7d898ae4633503e962a8
Oghenetega Esedere <arcteggzz@gmail.com>
2022-06-20 20:23:38 +0100

    Landing Page Deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
-// import LandingPage from "./Components/LandingPage/LandingPage"
+import LandingPage from "./Components/LandingPage/LandingPage"
 // import AboutPage from "./Components/AboutPage/AboutPage"
-import UploadPage from "./Components/UploadPage/UploadPage"
+// import UploadPage from "./Components/UploadPage/UploadPage"
 // import MobileNavBarActive from "./Components/Utility/MobileNavBarActive"
 // import MobileNavBar from "./Components/Utility/MobileNavBar"
 
 function App() {
   return (
     <div className="App">
-      {/* <LandingPage /> */}
+      <LandingPage />
       {/* <AboutPage /> */}
       {/* <MobileNavBarActive /> */}
       {/* <MobileNavBar /> */}
-      <UploadPage />
+      {/* <UploadPage /> */}
     </div>
   );
 }
