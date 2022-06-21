0d4f66033281647ebd2d72319c4f55166a141efb
Oghenetega Esedere <arcteggzz@gmail.com>
2022-06-21 22:31:24 +0100

    Finished routing for Desktop

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import LandingPage from "./Components/LandingPage/LandingPage"
+import HomePage from "./Components/HomePage/HomePage"
 import AboutPage from "./Components/AboutPage/AboutPage"
 import UploadPage from "./Components/UploadPage/UploadPage"
+import PrivatePage from "./Components/PrivatePage/PrivatePage"
 // import MobileNavBarActive from "./Components/Utility/MobileNavBarActive"
 
 import { BrowserRouter, Routes, Route } from "react-router-dom"
@@ -11,7 +13,9 @@ function App() {
       <BrowserRouter >
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<HomePage />} />
           <Route path="/upload" element={<UploadPage />} />
+          <Route path="/private" element={<PrivatePage />} />
           <Route path="/project-team" element={<AboutPage />} />
         </Routes>
       </BrowserRouter>
---
+++ src/Components/PrivatePage/PrivatePage.jsx
@@ -0,0 +1,13 @@
+import NavBar from "../Utility/NavBar";
+import MobileNavBar from "../Utility/MobileNavBar";
+
+function PrivatePage() {
+    return (
+        <div className="">
+            <NavBar />
+            <MobileNavBar />
+            Hello Private Library Page, coming Soon
+        </div>
+    );
+}
+export default PrivatePage;
