63b40a21d0e028fc335608b01498393318c6ceb9
Oghenetega Esedere <arcteggzz@gmail.com>
2022-07-05 23:54:35 +0100

    Fetch data for HomePage

--- src/Components/HomePage/Home.jsx
+++ src/Components/HomePage/Home.jsx
@@ -1,14 +1,14 @@
 
-import Data from "./data/HomeData.js"
+// import Data from "./data/HomeData.js"
 import HomeCard from "./HomeCard";
 
-// import { useState } from "react";
-// import { ethers } from "ethers";
-// import contractABI from "../../constants/contractABI.json"
-// import contractAddress from "../../constants/constants.js"
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import contractABI from "../../constants/contractABI.json"
+import contractAddress from "../../constants/constants.js"
 
 function Home() {
-    // const [libraryFiles, setLibraryFiles] = useState([])
+    const [libraryFiles, setLibraryFiles] = useState([])
 
     // ..Ethers.js read function to check if address is an uploader
     // const fetchFiles = async () => {
@@ -16,24 +16,32 @@ function Home() {
     //     await provider.send("eth_requestAccounts", [])
     //     const decentralizedLibrary = new ethers.Contract(contractAddress, contractABI, provider)
     //     const contractFiles = await decentralizedLibrary._getListOfAllUploadedFiles()
-    //     console.table(contractFiles[0][4]);
-    //     // setLibraryFiles([])
-    //     // console.log(libraryFiles)
+    //     setLibraryFiles(contractFiles)
     // }
 
-    // fetchFiles()
+    useEffect( () => {
+        async function fetchFiles() {
+            const provider = new ethers.providers.Web3Provider(window.ethereum)
+            await provider.send("eth_requestAccounts", [])
+            const decentralizedLibrary = new ethers.Contract(contractAddress, contractABI, provider)
+            const contractFiles = await decentralizedLibrary._getListOfAllUploadedFiles()
+            setLibraryFiles(contractFiles)
+        }
+        fetchFiles()
+    }, [libraryFiles])
 
     return (
         <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-1 lg:mx-40 sm:mx-2">
-            {Data.map((data, index) => {
+            {libraryFiles.map((file, index) => {
                 return (
                     <HomeCard
                         key={index}
                         className="break-normal hover:shadow-red-400 hover:scale-105 transition-all"
-                        name = {data.name}
-                        uploader = {data.uploader}
-                        status = {data.status}
-                        link = {data.link}
+                        name = {file[1]}
+                        uploader = {file[2]}
+                        status = {file[5]}
+                        link = {file[4]}
+                        type = {file[6]}
                     />
                 )
             })}
